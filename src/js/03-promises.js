9c40cef70bc3b74d10f89ced371a3611e688c481
Maksymenko Maksym <maksymenko1992@ukr.net>
2022-08-17 22:23:51 +0300

    thanks to Yuriy :-)

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,71 +1,45 @@
-
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const refs = {
   firstDelay: document.querySelector('[name="delay"]'),
   step: document.querySelector('[name="step"]'),
   amount: document.querySelector('[name="amount"]'),
-
   submit: document.querySelector('[type="submit"]'),
 };
-
 const { firstDelay, step, amount, submit } = refs;
 
-/*
-Напиши скрипт, который при сабмите формы вызывает функцию 
-createPromise(position, delay) столько раз, 
-сколько ввели в поле amount. 
-При каждом вызове передай ей номер создаваемого промиса (position) 
-и задержку учитывая введенную пользователем первую задержку (delay) и шаг (step).
-*/
-
-submit.addEventListener('click', createPromise)
-
-
-function createPromise(posiion, { value: fD } = firstDelay) {
+const onClick = (event) => {
   event.preventDefault();
-  const { value: stepDelay } = step;
-  const { value: number } = amount;
 
-  let n = 0;
-  if (+number <= 0) {
-    return
+  for (let i = 0; i < amount.value; i++) {
+    const steps = +step.value * i;
+    const ourDelay = +firstDelay.value + steps;
+    createPromise(i, ourDelay).then(data => {
+      return console.log(data)
+    }).catch(v => {
+      console.log(v)
+    })
   }
+};
 
-  setTimeout(() => {
-    const shouldResolve = Math.random() > 0.3;
+submit.addEventListener('click', onClick)
 
-    n += 1;
-
-    if (shouldResolve) {
-      // Fulfill
-      console.log(`Goodaaa !!!!!!!${n}`);
-    } else {
-      // Reject
-      console.log(`OOPS${n}`);
-    };
-
-  }, +fD)
-
+function createPromise(num, delay) {
+  const prom = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve(Notify.failure(`Fulfilled promise ${num + 1} in ${delay}ms`));
+      }
+      else {
+        reject(Notify.success(`Rejected promise ${num + 1} in ${delay}ms`));
+      }
+    }, delay)
 
 
-  const idTimer = setInterval(() => {
-    const shouldResolve = Math.random() > 0.3;
+  })
 
-    n += 1;
-
-    if (n === +number) {
-      clearInterval(idTimer);
-    }
-
-    if (shouldResolve) {
-      // Fulfill
-      console.log(`Goodaaa !!!!!!!${n}`);
-    } else {
-      // Reject
-      console.log(`OOPS${n}`);
-    };
-
-  }, (+fD + +stepDelay))
-
+  return prom
 }
+
 
